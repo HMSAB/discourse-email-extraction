fe05d6a6ca9fb0d1e97c1295c3a02a19c42e25a3
jose-hms <jose@hms.se>
2018-03-16 11:19:06 -0400

    Functionality to create and notify users

--- plugin.rb
+++ plugin.rb
@@ -1,10 +1,10 @@
-
 # name: email-extraction
 # about: Extract and invite CC'd users to topic
 # version: 0.1
 # authors: Jordan Seanor
 # url: https://github.com/HMSAB/discourse-email-extraction.git
 
+require_relative 'new_user_extraction_email'
 
 enabled_site_setting :email_extraction_enabled
 
@@ -12,26 +12,36 @@ after_initialize do
 
   class ::ExtractionHandler
 
-    # Create or extract existing users.
-    def self.create_or_find_users(emails, op_id)
+    # Create or extract existing users. If  you have
+    # not opted to create a full user, instead create
+    # a staged user.
+    def self.create_or_find_users(emails, topic_id)
       @users = []
-      if emails.size > 0
+      max_users = 0
+      if !emails.nil? && emails.size > 0
         emails.each do |address|
+        if max_users < SiteSetting.email_extraction_max_users && SiteSetting.email_extraction_should_invite
           if address != SiteSetting.pop3_polling_username
             user = User.find_by_email(address)
             if user.nil? && SiteSetting.enable_staged_users
               begin
                 if SiteSetting.email_extraction_create_full_user
-                  random_password = Array.new(15){[*"A".."Z", *"0".."9", *"!".."?"].sample}.join
-                  puts random_password
+                  while true
+                    random_password = Array.new(15){[*"A".."Z", *"0".."9"].sample}.join
+                    username = ::ExtractionHandler.generate_random_user()
+                    if !User.find_by_username(username) # Ensure we don't get a duplicate username.
+                      break;
+                    end
+                  end
                   user = User.create(
                     email: address,
-                    username: ::ExtractionHandler.generate_random_user(),
+                    username: username,
                     name: User.suggest_name(address),
                     password: random_password
                   )
                   user.approve(Discourse.system_user, true)
                   user.activate
+                  ::ExtractionHandler.send_user_email(user.email, user.username, random_password, topic_id)
                 else
                   user = User.create!(
                     email: address,
@@ -45,11 +55,17 @@ after_initialize do
                 puts e.backtrace.inspect
                 user = nil
               end
+            else
+              ::ExtractionHandler.send_user_email(user.email, user.username, nil, topic_id)
             end
           end
           @users << user
+          max_users += 1
+        else
+          break
         end
-      end
+        end
+    end
       @users
     end
 
@@ -62,24 +78,62 @@ after_initialize do
     # Extract and invite new email users
     def self.extract_email_details(entry)
       if !entry.raw_email.empty?
-        @mail = Mail.new(entry.raw_email)
-        @to_users = ::ExtractionHandler.create_or_find_users(@mail.to&.map(&:downcase), entry.user_id)
-        @cc_users = ::ExtractionHandler.create_or_find_users(@mail.cc&.map(&:downcase), entry.user_id)
-        max_users = 0
-        @cc_users.each do |user|
-          if max_users < SiteSetting.email_extraction_max_users
-            entry.topic.topic_allowed_users.create!(user_id: user.id)
-            TopicUser.auto_notification_for_staging(user.id, entry.topic_id, TopicUser.notification_reasons[:auto_watch])
-            entry.topic.add_small_action(Discourse.system_user, "invited_user", user.username)
-            max_users += 1
+        begin
+          @mail = Mail.new(entry.raw_email)
+          @all_emails = []
+          #cc'd users aren't included in the body so we have to pull those manually.
+          @cc_users = @mail.cc&.map(&:downcase)
+          if !@cc_users.nil?
+            @all_emails = @all_emails + @cc_users
+          end
+
+          @body_emails = @mail.body.to_s.upcase.scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/).uniq
+          @body_emails = ::ExtractionHandler.delete_email_element(@mail.to.to_s.upcase, @mail.from.to_s.upcase, @body_emails)
+
+          if !@body_emails.nil?
+            @all_emails = @all_emails + @body_emails
+          end
+
+          @all_users = ::ExtractionHandler.create_or_find_users(@all_emails, entry.topic_id)
+          if @all_users.length > 0
+            @all_users.each do |user|
+              entry.topic.topic_allowed_users.create!(user_id: user.id)
+              TopicUser.auto_notification_for_staging(user.id, entry.topic_id, TopicUser.notification_reasons[:auto_watch])
+              entry.topic.add_small_action(Discourse.system_user, "invited_user", user.username)
           end
         end
+      rescue Exception => e
+        puts e.message
+        puts e.inspect
+      end
       end
     end
 
+    #In certain scenarios, especially outlook clients, duplicate emails
+    #are created with a substring of the existing emails. Here we just
+    #remove anything in from/to and any variation of that.
+    def self.delete_email_element(to_email, from, array)
+      array.delete_if{ |email| email == to_email }
+      array.delete_if{ |email| email == from }
+      array.delete_if{ |email| to_email.include?(email)}
+      array.delete_if{ |email| from.include?(email)}
+      array
+    end
 
+    #Determien if we should allow extracting emails
     def self.extract_allowed?(entry)
       SiteSetting.email_extraction_enabled
+    end
+
+    #Send user creation email
+    def self.send_user_email(to_address, username, password, topic_id)
+      if SiteSetting.email_extraction_notify_users
+        if password.nil?
+          ExtractionEmailing::NewUserExtractionEmail.new.execute(template: 'email_extracted_existing_user', to_address: to_address, topic_id: topic_id)
+        else
+          ExtractionEmailing::NewUserExtractionEmail.new.execute(template: 'email_extracted_new_user', to_address: to_address, target_username: username, random_password: password, topic_id: topic_id)
+        end
+      end
     end
   end
 
